d0bd5a221316e501d4638b3d3da63908a8e9a939
Thaissa Rojas <thaissarojas@192.168.0.19>
2026-08-10 20:22:55 -0300

    chaned call to action buttons

--- src/app/contact/contact.ts
+++ src/app/contact/contact.ts
@@ -6,4 +6,44 @@ import { Component } from '@angular/core';
   templateUrl: './contact.html',
   styleUrl: './contact.css',
 })
-export class Contact {}
+export class Contact {
+  private readonly emailParts = [
+    's',
+    'a',
+    'n',
+    'd',
+    'r',
+    'a',
+    'r',
+    'o',
+    'j',
+    'a',
+    's',
+    't',
+    'r',
+    'a',
+    'd',
+    'u',
+    'c',
+    'a',
+    'o',
+    '@',
+    'g',
+    'm',
+    'a',
+    'i',
+    'l',
+    '.',
+    'c',
+    'o',
+    'm',
+  ];
+
+  readonly email = this.emailParts.join('');
+  readonly mailtoHref = `mailto:${this.email}?subject=Orçamento%20de%20tradução%20juramentada`;
+
+  openEmail(event: Event): void {
+    event.preventDefault();
+    window.location.href = this.mailtoHref;
+  }
+}
